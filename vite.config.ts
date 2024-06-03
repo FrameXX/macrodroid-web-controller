626317342baabe16c940d13ad6490ffced7b3453
FrameXX <jirkakral978@gmail.com>
2024-06-03 15:09:02 +0200

    Update gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    assetsInlineLimit: 0,
     rollupOptions: {
       input: {
         app: "./src/index.html",
